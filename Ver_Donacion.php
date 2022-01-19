2df7f15be796832a04084682d099d2a6aac866eb
jayvi95 <74685024+jayvi95@users.noreply.github.com>
2022-01-19 07:09:27 -0600

    cambioagrega donacion

--- Ver_Donacion.php
+++ Ver_Donacion.php
@@ -74,8 +74,8 @@ $con = conectar();
                     <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
                         <thead>
                             <tr>
-                                <th>Dui</th>
-                                <th>Nombre</th>
+                                <th>ID</th>
+                                <th>Donacion</th>
                                 <th>Apellido</th>
                                 <th>Fecha Nácimiento</th>
                                 <th>Fecha Contrato</th>
